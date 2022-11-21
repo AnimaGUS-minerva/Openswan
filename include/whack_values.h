5242ffbd97858d2ec564874452ba90f9cc323603
Michael Richardson <mcr@sandelman.ca>
2022-11-21 16:23:57 -0500

    updated whack_values from updated whack.cddl

--- include/whack_values.h
+++ include/whack_values.h
@@ -1,4 +1,12 @@
 /* generated from whack.cddl */
+
+/*
+; this is a CDDL file that defines the API from other programs into
+; pluto for loading policy.
+; This file is converted by the "cddlc" program into include/whack_values.h
+; This is a manual process, do: cddlc --to=enum whack.cddl >whack_values.h
+*/
+
 #ifndef WHACKVALUES_H
 #define WHACKVALUES_H
 enum whack_message_keys {
@@ -125,4 +133,5 @@ enum initiateoppo_keys {
   WHACK_OPT_OPPO_MY_CLIENT = 143,
   WHACK_OPT_OPPO_PEER_CLIENT = 144,
 };
+
 #endif /* WHACKVALUES_H */
