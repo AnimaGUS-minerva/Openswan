3652e817530435f29466ddafb22add502cff5d6c
Michael Richardson <mcr@sandelman.ca>
2022-12-25 14:30:58 -0500

    recipe for generating whack_values.h

---
+++ docs/whack_values_head.h
@@ -0,0 +1,11 @@
+/* generated from whack.cddl */
+
+/*
+; this is a CDDL file that defines the API from other programs into
+; pluto for loading policy.
+; This file is converted by the "cddlc" program into include/whack_values.h
+; This is a manual process, do: cddlc --to=enum whack.cddl >whack_values.h
+*/
+
+#ifndef WHACKVALUES_H
+#define WHACKVALUES_H
---
+++ docs/whack_values_tail.h
@@ -0,0 +1,1 @@
+#endif /* WHACKVALUES_H */
